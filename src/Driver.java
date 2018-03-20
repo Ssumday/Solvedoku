01936e67e226dec333b70ab0bcdd7ef5f95eba0c
Kevin Trang <ktrang3@calstatela.edu>
2018-03-19 19:25:33 -0700

    FileReader and ImageViewer showed

--- src/Driver.java
+++ src/Driver.java
@@ -1,18 +1,45 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import java.util.Optional;
+
 public class Driver extends Application{
+
+    FileReader image = new FileReader();
     public static void main(String[] args) {
         launch(args);
     }//end of main
 
+
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FileReader fileR = new FileReader();
         StackPane pane = new StackPane();
         Scene scene = new Scene(pane, 700, 660);
-        primaryStage.setTitle("Choice Giveaways");
+        primaryStage.setTitle("Solvedoku");
+        ImageView view = new ImageView();
+        Image image = new Image(FileReader.FileChooser());
+
+        view.setImage(image);
+        view.setFitHeight(500);
+        view.setFitWidth(500);
+
+        pane.getChildren().add(view);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Image");
+        alert.setHeaderText(null);
+        alert.setContentText("This is the image you have entered.");
+
+        alert.showAndWait();
     }//end of start
+
+
 }//end of application
--- src/FileReader.java
+++ src/FileReader.java
@@ -1,12 +1,20 @@
+import javafx.scene.image.Image;
 import javafx.stage.FileChooser;
 import javafx.scene.layout.BorderPane;
 import java.io.BufferedReader;
 import java.io.File;
 
 public class FileReader extends BorderPane{
-    public static void FileChooser(){
+    public static String FileChooser(){
         FileChooser fileChoose = new FileChooser();
-        fileChoose.setTitle("Choose a text file");
-
+        fileChoose.setTitle("Choose an image file");
+        File data = fileChoose.showOpenDialog(null);
+        if(data != null){
+            String imagePath = data.toURI().toString();
+            return imagePath;
+        }//end of if
+        else{
+            return null;
+        }
     }
 }//end of FileReader
