1d030cc56368a1278594159e3347fa88dfbb41e5
Allen Ruan <yilinruan@gmail.com>
2018-04-13 18:47:23 -0700

    Step 1; Adding OCR to the project;

---
+++ eclipse_tess4j_3.4 - Copy/Tess4J/src/test2.java
@@ -0,0 +1,27 @@
+
+import java.io.File;
+import java.io.IOException;
+
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
+public class test2 {
+	public static void main (String[] args) throws IOException{
+		// creating file instance and referencing the file in its location
+		File imageFile = new File("C:\\eclipse_tess4j_3.4\\Tess4J\\images\\eurotext.pdf");
+		
+		ITesseract instance = new Tesseract();
+		instance.setDatapath("C:/eclipse_tess4j/Tess4J/tessdata");
+		
+		try {
+			String result = instance.doOCR(imageFile);
+			System.out.println(result);
+		} catch(TesseractException e) {
+			System.err.println(e.getMessage());
+		}
+		
+	}
+	
+
+}
